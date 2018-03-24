1116740076e2a0e63bf9e59023182dd7c4d260aa
ShinokiRyosei <shinoki.r.0301@gmail.com>
2018-03-24 16:50:35 +0000

    fix review

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,7 @@
 class TasksController < ApplicationController
   
-  before_action :set_task, only: [:show, :edit, :update]
-  before_action :require_user_logged_in, only: [:new, :show, :index]
-  before_action :correct_user, only: [:destroy, :edit, :show]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy, :edit, :show, :update]
   
   def index
     if current_user
@@ -52,10 +51,6 @@ class TasksController < ApplicationController
   end
   
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   def task_params
     params.require(:task).permit(:content, :status)
